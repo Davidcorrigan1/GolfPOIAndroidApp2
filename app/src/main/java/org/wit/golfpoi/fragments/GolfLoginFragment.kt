a44244e2558594890751aad97d29ada4481c43c4
David Corrigan <davidcorrigan1@gmail.com>
2021-11-22 20:55:08 +0000

    Initial Changes for Firebase Auth functionality

--- app/src/main/java/org/wit/golfpoi/fragments/GolfLoginFragment.kt
+++ app/src/main/java/org/wit/golfpoi/fragments/GolfLoginFragment.kt
@@ -1,18 +1,24 @@
 package org.wit.golfpoi.fragments
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import org.wit.golfpoi.R
 import org.wit.golfpoi.databinding.FragmentGolfLoginBinding
 import org.wit.golfpoi.main.MainApp
 import org.wit.golfpoi.models.GolfUserModel
+import timber.log.Timber
 import timber.log.Timber.i
 
 
@@ -21,6 +27,8 @@ class GolfLoginFragment : Fragment() {
     lateinit var app: MainApp
     private var _fragBinding: FragmentGolfLoginBinding? = null
     private val fragBinding get() = _fragBinding!!
+    private lateinit var auth: FirebaseAuth
+    lateinit var loader : AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +44,18 @@ class GolfLoginFragment : Fragment() {
         _fragBinding = FragmentGolfLoginBinding.inflate(inflater, container, false)
         val root = fragBinding?.root
 
+        auth = FirebaseAuth.getInstance()
+
         setButtonListener(fragBinding)
 
         return root
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
     }
 
     companion object {
@@ -53,7 +69,13 @@ class GolfLoginFragment : Fragment() {
     fun setButtonListener(layout: FragmentGolfLoginBinding) {
 
         layout.btnLogin.setOnClickListener {
-            i("Check the user exists and check password")
+            i("Check the user exists or Firebase")
+
+            signIn(layout.editTextEmail.text.toString(), layout.editTextPassword.text.toString())
+
+            // need to include the below someone in signIn above!
+
+
             var loggedInUser: GolfUserModel? =
                 app.golfPOIData.findUser(layout.editTextEmail.text.toString())
 
@@ -96,6 +118,69 @@ class GolfLoginFragment : Fragment() {
     override fun onResume() {
         super.onResume()
     }
+
+    private fun signIn(email: String, password: String) {
+        Timber.d( "signIn:$email")
+        if (!validateForm()) {
+            return
+        }
+
+
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(activity?.mainExecutor!!) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Timber.d( "signInWithEmail:success")
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Timber.w( "signInWithEmail:failure $task.exception")
+                    Toast.makeText(activity, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+
+                // [START_EXCLUDE]
+                if (!task.isSuccessful) {
+                    Toast.makeText(activity, R.string.login_error_message,
+                        Toast.LENGTH_LONG).show()
+                }
+
+                // [END_EXCLUDE]
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun signOut() {
+        auth.signOut()
+    }
+
+
+
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = fragBinding.editTextEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            fragBinding.editTextEmail.error = "Required."
+            valid = false
+        } else {
+            fragBinding.editTextEmail.error = null
+        }
+
+        val password = fragBinding.editTextPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            fragBinding.editTextPassword.error = "Required."
+            valid = false
+        } else {
+            fragBinding.editTextPassword.error = null
+        }
+
+        return valid
+    }
+
 
 
 }
